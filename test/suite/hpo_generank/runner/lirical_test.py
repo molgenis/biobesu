c4597512713ace2ded34b7c3d0cde1f3935be5a6
Sander van den Hoek <savandenhoek@gmail.com>
2021-03-04 13:04:22 +0100

    Updated a unit test.

--- test/suite/hpo_generank/runner/lirical_test.py
+++ test/suite/hpo_generank/runner/lirical_test.py
@@ -1,6 +1,6 @@
 #!/user/bin/env python3
 
-from biobesu.suite.hpo_generank.runner.lirical import __extract_genes_from_lirical_data
+from biobesu.suite.hpo_generank.runner.lirical import __extract_fields_from_lirical_data
 
 
 def test_lirical_file_digestion():
@@ -11,7 +11,7 @@ def test_lirical_file_digestion():
                     '200\ta Syndrome\tOMIM:848484\t1/7987\t0,00%\t0\tn/a\tn/a\n',
                     '450\tJust something more; BDBS5\tOMIM:112358\t1/7987\t0,00%\t0\tn/a\tn/a\n']
 
-    expected_output = ['ABC1', 'BDBS5']
-    actual_output = __extract_genes_from_lirical_data(input_string)
+    expected_output = (['ABC1', 'BDBS5'],['123456', '848484', '112358'])
+    actual_output = __extract_fields_from_lirical_data(input_string)
 
     assert actual_output == expected_output
