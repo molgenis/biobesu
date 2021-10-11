25e1ec220d9d1d31174c4283c757ccbf1f37a26b
Sander van den Hoek <savandenhoek@gmail.com>
2021-10-11 11:19:48 +0200

    Fixed some tests.

--- test/suite/vibe_versions/helper/vibe_versions-converters_test.py
+++ test/suite/vibe_versions/helper/vibe_versions-converters_test.py
@@ -7,7 +7,7 @@ def test_list_to_vibe_arguments_empty_list():
     input_list = []
 
     expected_output = ''
-    actual_output = converters.list_to_vibe_arguments(input_list, '-m')
+    actual_output = converters.convert_list_to_arguments_with_same_key(input_list, '-m')
 
     assert actual_output == expected_output
 
@@ -15,7 +15,7 @@ def test_list_to_vibe_arguments_single_item():
     input_list = ['OMIM:012345']
 
     expected_output = '-m OMIM:012345'
-    actual_output = converters.list_to_vibe_arguments(input_list, '-m')
+    actual_output = converters.convert_list_to_arguments_with_same_key(input_list, '-m')
 
     assert actual_output == expected_output
 
@@ -23,6 +23,6 @@ def test_list_to_vibe_arguments_list():
     input_list = ['OMIM:012345', 'OMIM:543210']
 
     expected_output = '-m OMIM:012345 -m OMIM:543210'
-    actual_output = converters.list_to_vibe_arguments(input_list, '-m')
+    actual_output = converters.convert_list_to_arguments_with_same_key(input_list, '-m')
 
     assert actual_output == expected_output
