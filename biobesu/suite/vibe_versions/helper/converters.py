80c22c82465dabcccedfd61dd142fa22ac700439
Sander van den Hoek <savandenhoek@gmail.com>
2021-10-12 16:41:49 +0200

    Improved formatting of python code (line width).

--- biobesu/suite/vibe_versions/helper/converters.py
+++ biobesu/suite/vibe_versions/helper/converters.py
@@ -13,12 +13,15 @@ def convert_list_to_arguments_with_same_key(argument_list, argument):
 
 
 def merge_vibe_simple_output_files(vibe_dir, out_file):
-    with open(out_file, 'x') as file_writer:  # Requires creating a new file.
+    # Requires creating a new file.
+    with open(out_file, 'x') as file_writer:
         # Write header.
         file_writer.write("id\tsuggested_genes\n")
         for vibe_out_file in listdir(vibe_dir):
-            if not vibe_out_file.startswith('.'):  # Ignore hidden files such as .DS_Store.
+            # Ignore hidden files such as .DS_Store.
+            if not vibe_out_file.startswith('.'):
                 with open(vibe_dir + vibe_out_file) as file_reader:
                     file_name = vibe_out_file.split('.')[0]
-                    genes = file_reader.readline()  # File should contain single comma separated line.
+                    # File should contain single comma separated line.
+                    genes = file_reader.readline()
                     file_writer.write(f'{file_name}\t{genes}\n')
